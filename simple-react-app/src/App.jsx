ff209a055ac750180db75264717ae348e5e1e261
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2025-11-08 18:15:05 -0500

    Componente Contador

---
+++ simple-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Counter } from './components/Counter'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>useState</h1>
+      <Counter />
+    </>
+  )
+}
+
+export default App
