aec100266f9ef784d5838f667112873351391201
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2025-11-08 15:51:15 -0500

    Ejercicio base, lista js a html ul

--- platziworld/src/App.jsx
+++ platziworld/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 
 function App() {
+  const items = ["React", "Javascript", "Vite"];
 
   return (
-    <>
-      <h1>Hi world!</h1>
-    </>
+    <section>
+      <h1>Hi react!</h1>
+      <p>I'm learning about...</p>
+      <ul>
+        {
+          items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))
+        }
+      </ul>
+    </section>
   )
 }
 
