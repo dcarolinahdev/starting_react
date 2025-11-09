2336a0d33a40549aebb8b6cbbef3f481e3a885a2
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2025-11-09 12:13:45 -0500

    Estilos en componentes - n1

---
+++ styles-react-app/src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+//import "./styles.css"
+import styles from './Card.module.css'
+
+export const Card = () => {
+    return (
+        <div className={styles.card}>
+            Soy una card !
+        </div>
+    )
+}
